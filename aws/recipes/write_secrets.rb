911c3160e4fbf30e7d8308396bee5282725ff475
JP Slavinsky <jpslav@gmail.com>
2013-05-06 16:36:29 -0700

    commenting out

--- aws/recipes/write_secrets.rb
+++ aws/recipes/write_secrets.rb
@@ -1,12 +1,12 @@
 
 
-template "/etc/sudoers" do
-  source "sudoers.erb"
-  mode 0440
-  owner "root"
-  group "root"
-  variables({
-     :sudoers_groups => node[:authorization][:sudo][:groups],
-     :sudoers_users => node[:authorization][:sudo][:users]
-  })
-end
+# template "/etc/sudoers" do
+#   source "sudoers.erb"
+#   mode 0440
+#   owner "root"
+#   group "root"
+#   variables({
+#      :sudoers_groups => node[:authorization][:sudo][:groups],
+#      :sudoers_users => node[:authorization][:sudo][:users]
+#   })
+# end
