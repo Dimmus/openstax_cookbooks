11a1cb06472cab0d65adf25b3f6c398e538d2711
JP Slavinsky <jpslav@gmail.com>
2013-05-09 14:19:54 -0700

    deleted comments and OBE web_server.rb

--- openstax_exchange/recipes/web_server.rb
+++
@@ -1,7 +0,0 @@
-Chef::Log.info("Starting web_server recipe")
-
-if (node[:instance_role] == 'vagrant')
-  include_recipe "aws::opsworks_custom_layer_setup"
-end
-
-include_recipe "openstax_exchange::web_server_setup"
