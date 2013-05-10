9f122afa77fe3ae8400ddbb21c85e9b8a5c4c4b4
JP Slavinsky <jpslav@gmail.com>
2013-05-10 11:56:10 -0700

    trying to accomodate pip in chef 0.9

--- aws/recipes/default.rb
+++ aws/recipes/default.rb
@@ -1,9 +1,13 @@
 include_recipe "python::package"
 include_recipe "python::pip"
 
-python_pip "awscli" do
-  action :install
+execute "install awscli manually because AWS chef is ancient" do
+  command "pip install awscli"
 end
+
+# python_pip "awscli" do
+#   action :install
+# end
 
 if (node["aws"]["use_cli_mockup"]) 
   Chef::Log.info("Masking real aws cli program with mockup")
--- aws/recipes/deregister.rb
+++ aws/recipes/deregister.rb
@@ -1,11 +1,12 @@
 include_recipe "aws"
 
-execute "deregister" do
-  command <<-EOS \
-    aws --region #{node[:opsworks][:instance][:region]} \
-        elb deregister-instances-from-load-balancer \
-        --load-balancer-name #{node[:aws][:load_balancer_name]} \
-        --instances '{\"instance_id\":\"#{node[:opsworks][:instance][:aws_instance_id]}\"}'
-  EOS
-  user "deploy"
+Chef::Log.info("Will disconnect node (#{node[:opsworks][:instance][:aws_instance_id]}) from " + 
+               "load balancer '#{node[:aws][:load_balancer_name]}' in region '#{node[:opsworks][:instance][:region]}'")
+
+execute "register" do
+  command "aws --region #{node[:opsworks][:instance][:region]} \
+               elb deregister-instances-from-load-balancer \
+               --load-balancer-name #{node[:aws][:load_balancer_name]} \
+               --instances '{\"instance_id\":\"#{node[:opsworks][:instance][:aws_instance_id]}\"}'"
+  user "#{node[:aws][:execute_user]}"
 end
