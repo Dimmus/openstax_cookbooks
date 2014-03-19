61f2dfe6def8937827d08a6ca4e947b7151e7188
JP Slavinsky <jpslav@gmail.com>
2014-03-19 12:54:41 -0700

    removed top level metadata file

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'openstax_cookbooks'
-maintainer       'Rice University'
-maintainer_email 'jps@kindlinglabs.com'
-license          'MIT License'
-description      'Chef configuration for OpenStax Products'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
